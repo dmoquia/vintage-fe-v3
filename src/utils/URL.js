1069733e03ff2c34e9e24759737eada59c3de262
dmoquia <dennis.moquia@gmail.com>
2023-02-28 15:25:07 +0800

    update url.js

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,4 +1,4 @@
 // url
 // eslint-disable-next-line import/no-anonymous-default-export
-// export default "https://vintage-be.up.railway.app";
-export default "http://localhost:1337";
+export default "https://vintage-be.up.railway.app";
+// export default "http://localhost:1337";
